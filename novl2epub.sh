675651e22fbc81fdf26913871e2fdc8d322e01d8
vakhdmyt06 <59621875+vakhdmyt06@users.noreply.github.com>
2022-10-30 12:50:41 +0100

    Update novl2epub.sh

--- novl2epub.sh
+++ novl2epub.sh
@@ -10,7 +10,7 @@ echo "Dmytro Vakhitov"
 echo "  LINK=$arg_link\n  ARG=$arg_maxchap$arg_skipimg"
 if [ $# -eq "0" ]; then 
   echo "ERROR"
-  echo "SYNTAX: $0 <https://lightnovels.net/nameofnovel> <amount of \$i> <SKIP IMAGE DOWNLOAD>"
+  echo "SYNTAX: bash $0 <https://lightnovels.net/nameofnovel> <amount of \$i> <SKIP IMAGE DOWNLOAD>"
   echo "\nSKIP IMAGE DOWNLOAD:  The script fixes images inside the html/ebook be replacing the link with a downloaded image. make <SKIP IMAGE DOWNLOAD> \'1\' if you wish to skip that."
   #echo "\nUNUSED:BEHAVIOR  |  COUNT TYPE\n--------\n1  |  /volume-1.html  : 1 counting up until <amount of \$i>\n1  |  /chapter-1.html  : 1 counting up until <amount of \$i>\n2  |  /volume-1-chapter-2.html  : 2 counting up until [maximal of \$j], then 1 +1, then repeat until 1 is <amount of \$i>\n3  |  /volume-1-chapter-2.html  : 1 counting up until [maximal of \$j], then 2 +1, then repeat until 2 is <amount of \$i> \n"
   #echo "UNUSED:Syntax: https://lightnovels.net/nameofnovel 20 2 volume-\$i-chapter-\$j\nSyntax: https://lightnovels.net/nameofnovel 10 1 volume-\$i\n"
