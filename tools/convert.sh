fe9819dc256dd45ccffac3facef0478de3ef649d
vakhdmyt06 <vakhdmyt06@gmail.com>
2022-12-12 19:31:55 +0100

    Fix page ending

--- tools/convert.sh
+++ tools/convert.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo "# cutting and converting 'html' to 'epub'"
 mv out .out.old
+rm -r out
+rm -r temp
 mkdir temp
 mkdir out
 chap=$1
@@ -10,8 +12,8 @@ do
   MYASS=$i
   echo "- Volume $MYASS"
   cp html/volume-$MYASS.html temp/volume-$MYASS.temp
-  sed -i 's/><\/div>/><\/div>\n<p>PAGE<\/p>/g' temp/volume-$MYASS.temp
-  sed -e '1,/<div class="chapter-content">/ d;/<ol>/Q' temp/volume-$MYASS.temp > temp/vol-$MYASS.nvl
+  sed -i 's/><\/div>/><\/div>\n<p>Do regular breaks!<\/p>/g' temp/volume-$MYASS.temp
+  sed -e '1,/<div class="chapter-content">/ d;/<!-- AI CONTENT END 2 -->/Q' temp/volume-$MYASS.temp > temp/vol-$MYASS.nvl
   echo "making epub"
   cd html
   pandoc --self-contained -f  html -t epub3 --metadata title="Novel2Ebook Novel Vol. $MYASS" -o ../out/vol$MYASS.epub ../temp/vol-$MYASS.nvl
